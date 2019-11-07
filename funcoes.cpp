18e2ee23c4d37ea501af9e77205e45d058389bef
htmluke <lucas.emanoell@outlook.com>
2019-11-07 14:17:38 -0300

    Adicionando as funçoes genericas

---
+++ funcoes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include "classes.hpp"
+#include "funcoes.hpp"
+
+using namespace std;
+
+void menu(){
+    cout << "Escolha uma opção: " << endl;
+    cout << "1 - Criar Tabela " << endl;
+    cout << "2 - Listar Tabelas Existentes" << endl;
+    cout << "3 - Adicionar Nova Linha na Tabela" << endl;
+    cout << "4 - Listar Todos os Dados da Tabela" << endl;
+    cout << "5 - Pesquisar Valor na Tabela" << endl;
+    cout << "6 - Apagar Valor na Tabela" << endl;
+    cout << "7 - Excluir Tabela" << endl;
+}
