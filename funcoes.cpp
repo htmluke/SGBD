790e439b20e9659ca41e47f6bfa26d8ba999475f
htmluke <lucas.emanoell@outlook.com>
2019-11-07 19:57:12 -0300

    Adicionando enum e funçao para a escolha das opcoes

--- funcoes.cpp
+++ funcoes.cpp
@@ -14,4 +14,55 @@ void menu(){
     cout << "5 - Pesquisar Valor na Tabela" << endl;
     cout << "6 - Apagar Valor na Tabela" << endl;
     cout << "7 - Excluir Tabela" << endl;
+    cout << "0 - Sair" << endl;
+}
+
+typedef enum Opcoes{
+	SAIR,
+	CRIARTABELA,
+	LISTARTABELA,
+	ADICIONARLINHA,
+	LISTARDADOS,
+	PESQUISARVALOR,
+	APAGARVALOR,
+	EXCLUIRTABELA
+} Opcoes;
+
+void opcaoMenu()
+{
+    int aux = 1, opcao;
+    Opcoes op;
+    cin >> opcao;
+
+    switch (op){
+        case SAIR:
+            aux = 0;
+            cout << "Saindo..." << endl;
+            break;
+        case CRIARTABELA:
+
+            break;
+        case LISTARTABELA:
+
+            break;
+        case ADICIONARLINHA:
+
+            break;
+        case LISTARDADOS:
+
+            break;
+        case PESQUISARVALOR:
+
+            break;
+        case APAGARVALOR:
+
+            break;
+        case EXCLUIRTABELA:
+
+            break;
+        default:
+            cout << "Opção inválida, digite novamente :)" << endl;
+            break;
+    }
+    if(aux == 1) opcaoMenu();
 }
