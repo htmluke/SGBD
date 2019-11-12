d696f803be53f547e8851a690316afa0473c216b
htmluke <lucas.emanoell@outlook.com>
2019-11-12 15:09:29 -0300

    Adicionando funçoes de listar

--- src/funcoes.cpp
+++ src/funcoes.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <list>
 #include "classes.hpp"
 #include "funcoes.hpp"
 
@@ -15,4 +16,43 @@ void menu(){
     cout << "6 - Apagar Valor na Tabela" << endl;
     cout << "7 - Excluir Tabela" << endl;
     cout << "8 - Sair" << endl;
+}
+
+void inserirLista(string nome, list<string> lista){
+    if(lista.empty() == true){
+        lista.push_front(nome);
+    }
+    else{
+        lista.push_back(nome);
+    }
+}
+void listarTabela(list<string> lista){
+    if(lista.empty() == true){
+        cout << "A lista se encontra vazia, insira uma tabela para poder listá-la." << endl;
+    }
+    else{
+        int tamanho = lista.size();
+        for (int i = 0; i < tamanho; i++)
+        {
+            cout << lista.front() << endl;
+        }
+    }
+}
+void excluirNomeTabela(list<string> lista){
+    string nome;
+    cout << "Digite o nome da tabela que deseja retirar da lista:" << endl;
+    cin >> nome;
+    if(lista.empty() == true){
+        cout << "Não existem tabelas para a ação, crie uma tabela." << endl;
+    }
+    else{
+        int tamanho = lista.size();
+        for (int i = 0; i < tamanho; i++){
+            if(nome == lista.front()){
+                lista.pop_front();
+                cout << "Nome excluído" << endl;
+                break;
+            }
+        }
+    }
 }
