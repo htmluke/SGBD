5f4588714022f55d3f05b96787e0dd031df2e2e3
htmluke <lucas.emanoell@outlook.com>
2019-11-12 15:10:43 -0300

    Estabelecendo sistema de escolha (problema ao passar os parametros pro construtor

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,72 @@
 #include <iostream>
 #include <string>
+#include <list>
 #include "classes.hpp"
 #include "funcoes.hpp"
 
 using namespace std;
 
+typedef enum Opcoes{
+	SAIR,
+	CRIARTABELA,
+	LISTARTABELA,
+	ADICIONARLINHA,
+	LISTARDADOS,
+	PESQUISARVALOR,
+	APAGARVALOR,
+	EXCLUIRTABELA
+} Opcoes;
+
 int main(int argc, const char * argv[]){
+    list<string> lista;
     Tabela T;
+    Opcoes op;
     int cP = 1, cE = 2, n = 3;
-    Tabela f(cP, cE, n);
-    menu();
+    int aux = 1, opcao;
+    while(aux == 1){
+        //Imprime o menu toda vez que o usuário for realizar uma nova atividade
+        menu();
+        cin >> opcao;
+        switch (op){
+
+            case SAIR:
+                aux = 0;
+                cout << "Saindo..." << endl;
+                break;
+
+            case CRIARTABELA:
+                Tabela T(cP, cE, n, lista);
+                break;
+
+            case LISTARTABELA:
+
+                break;
+
+            case ADICIONARLINHA:
+
+                break;
+
+            case LISTARDADOS:
+
+                break;
+
+            case PESQUISARVALOR:
+
+                break;
+
+            case APAGARVALOR:
+
+                break;
+
+            case EXCLUIRTABELA:
+
+                break;
+
+            default:
+                cout << "Opção inválida, digite novamente :)" << endl;
+                break;
+        }
+    }
+    
     return 0;
 }
