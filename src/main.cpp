dc5ca1a5fc468588edfc3c7edaf5e16545bc162e
lucasx7 <lucasolix7@gmail.com>
2019-11-18 20:32:46 -0300

    adequando a classes.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -46,7 +46,7 @@ int main(int argc, const char * argv[]){
                 break;
 
            case LISTARTABELA:{
-                listarTabela("tabelas");            
+                listarTabelas("tabelas");            
             }
                 break;
 
